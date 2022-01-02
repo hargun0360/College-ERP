5ccf7429c8781f9ae3c94997f7217aa35e5cb29c
=Hargun Singh <=Hargun0360@gmail.com>
2022-01-02 20:42:52 +0530

    Add redux-thunk in store

--- Store.js
+++ Store.js
@@ -1,8 +1,9 @@
-import {createStore} from 'redux'
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from './Service/Reducer/index'
-
+import thunk from "redux-thunk";
+const middleware = [thunk];
 const store = createStore(
-    rootReducer,
+    rootReducer,applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   
