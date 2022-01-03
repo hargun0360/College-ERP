00d6675e991dd7eb115eabd89066904964a02033
=Hargun Singh <=Hargun0360@gmail.com>
2022-01-03 19:22:34 +0530

    Add Redux-Devtool-Extension in store

--- Store.js
+++ Store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from './Service/Reducer/index'
 import thunk from "redux-thunk";
+import { composeWithDevTools } from 'redux-devtools-extension';
 const middleware = [thunk];
 const store = createStore(
-    rootReducer,applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    rootReducer,composeWithDevTools(applyMiddleware(...middleware))
   );
   
   export default store;
