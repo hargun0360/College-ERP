c3789c1fbfd95a2da65bd53bcd391cb8259ce576
=Hargun Singh <=Hargun0360@gmail.com>
2022-01-31 01:50:46 +0530

    Holiday UI completed (Admin Page)

--- src/Pages/Dashboard/Admin/Holidays/Holidays.js
+++ src/Pages/Dashboard/Admin/Holidays/Holidays.js
@@ -1,33 +1,47 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Timeline from './Timeline'
 import { useSelector, useDispatch } from 'react-redux'
 import './Holiday.css'
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 const Holidays = () => {
-    const { val } = useSelector((state) => state.toggle);
-    const [value, onChange] = useState(new Date());
+  const { val } = useSelector((state) => state.toggle);
+  const [value, onChange] = useState(new Date());
   return (<>
 
-  <div className={`Holiday-Container ${val ? "activate" : ""}`}>
-  <div className='Holidays-calendar'>
-    <div className='Add-Holiday-btn'>
+    <div className={`Holiday-Container ${val ? "activate" : ""}`}>
+      <div className='Holidays-calendar'>
+        <div className='Calendar-card'>
+          <div className='holiday-card'>
+            <div className='Holiday-date'>
+              <h3>26</h3>
+              <span className='prefix'>th</span>
+              <span className='month-cal'>jan</span>
+            </div>
+            <div className='upcoming-Holiday'>
+              <h5>Upcoming Holiday</h5>
+            </div>
+            <div className='circular0'>
 
-    </div>
-    <div className='Calendar-card'>
-        <div className='holiday-card'>
-
-        </div>
-        <div className='Calendar'>
+            </div>
+          </div>
+          <div className='Calendar'>
             <Calendar onChange={onChange} value={value} />
+          </div>
+          <div className='Add-Holiday-btn'>
+            <div className='plus-icon'>
+              <box-icon name='plus-circle' color="#007BAB" ></box-icon>
+            </div>
+            <div className='Add-Student-text'>
+              <h4>Add Holiday</h4>
+            </div>
+          </div>
         </div>
-    </div>
-  </div>
-    <div className='timeline'>
+      </div>
+      <div className='timeline'>
         <Timeline />
+      </div>
     </div>
-  </div>
-  
+
   </>);
 };
 
