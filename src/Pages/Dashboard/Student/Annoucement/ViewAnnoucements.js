be354d83e49113794749f4945225b937f8557c71
=Hargun Singh <=Hargun0360@gmail.com>
2022-02-14 02:00:38 +0530

    View Admin Annoucement in Faculty Page

--- src/Pages/Dashboard/Student/Annoucement/ViewAnnoucements.js
+++ src/Pages/Dashboard/Student/Annoucement/ViewAnnoucements.js
@@ -1,9 +1,24 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { Paper, TableContainer, TableBody, Table, TableHead, TableCell, TableRow } from "@material-ui/core"
 const ViewAnnoucements = () => {
     const { val } = useSelector((state) => state.toggle);
+    const user = localStorage.getItem("userd");
     const [tableData, setTableData] = useState([{id:"1",date:"20-2-2022",time:"9:00 AM",Annby:"Hargun Singh",Ann:"so much data here"},{id:"2",date:"20-2-2022",time:"9:00 AM",Annby:"Hargun Singh",Ann:"so much data here"},{id:"3",date:"20-2-2022",time:"9:00 AM",Annby:"Hargun Singh",Ann:"so much data here"},{id:"4",date:"20-2-2022",time:"9:00 AM",Annby:"Hargun Singh",Ann:"so much data here"},{id:"5",date:"20-2-2022",time:"9:00 AM",Annby:"Hargun Singh",Ann:"so much data here"},{id:"6",date:"20-2-2022",time:"9:00 AM",Annby:"Hargun Singh",Ann:"so much data here"}])
+    const loadAnnF = () =>{
+        console.log("student");
+    }
+    const loadAnnS = () =>{
+            console.log("faculty");
+    }
+
+     useEffect(()=>{  
+        if(user === "student"){
+            loadAnnS();
+        }else if(user === "faculty"){
+            loadAnnF();
+        }
+    },[])  
   return (<>
       <div className={`Annoucement-Container ${val ? "activate" : ""}`} > 
       <TableContainer component={Paper} style={{
@@ -18,7 +33,9 @@ const ViewAnnoucements = () => {
                             <TableRow>
                                 <TableCell style={{ border: "0px solid transparent", width: "20%",fontFamily:"'Inter', sans-serif"  }} align='center'>Date</TableCell>
                                 <TableCell style={{ border: "0px solid transparent",fontFamily:"'Inter', sans-serif"  }} align='center'>Time</TableCell>
-                                <TableCell style={{ border: "0px solid transparent",fontFamily:"'Inter', sans-serif"  }} align='center'>Annoucement By</TableCell>
+                                {
+                                    user === "student" ? <TableCell style={{ border: "0px solid transparent",fontFamily:"'Inter', sans-serif"  }} align='center'>Annoucement By</TableCell> : null
+                                }
                                 <TableCell style={{ border: "0px solid transparent" ,fontFamily:"'Inter', sans-serif" }} align='center'>Annoucement</TableCell>
                             </TableRow>
                         </TableHead>
@@ -29,7 +46,9 @@ const ViewAnnoucements = () => {
                                     <TableRow>
                                         <TableCell style={{ border: "0px solid transparent",fontFamily:"'Inter', sans-serif"  }} align='center'>{data.date}</TableCell>
                                         <TableCell style={{ border: "0px solid transparent",fontFamily:"'Inter', sans-serif"  }} align='center'>{data.time}</TableCell>
-                                        <TableCell style={{ border: "0px solid transparent" ,fontFamily:"'Inter', sans-serif" }} align='center'>{data.Annby}</TableCell>
+                                        {
+                                            user === "student" ? <TableCell style={{ border: "0px solid transparent" ,fontFamily:"'Inter', sans-serif" }} align='center'>{data.Annby}</TableCell> : null
+                                        }
                                         <TableCell style={{ border: "0px solid transparent",fontFamily:"'Inter', sans-serif"  }} align='center'>{data.Ann}</TableCell>
                                     </TableRow>
                                 </>))
