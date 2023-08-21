306b5a5d28a55e8146e6b7cae5ae9bceaa0240a7
hargun0360 <hargunsingh0720@gmail.com>
2023-08-21 12:37:08 +0530

    deploy site

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SubmitButton = (props) => {
     return (
         <div>
-            <button className={props.className} type="sumbit">{props.Label}</button>
+            <button className={props.className} type="sumbit">{props.Label}</button> 
         </div>
     )
 }
